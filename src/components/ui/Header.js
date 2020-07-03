a9c083e1eaa08a332c9d0cd0cc496b8578031d3b
peter westman <pwestman@gmail.com>
2020-07-03 20:46:39 +0200

    adding components

---
+++ src/components/ui/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../../img/logo.png'
+
+const Header = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Header
